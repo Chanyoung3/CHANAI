2abc1335e8ae4f4e4cb1f12b718055f9a5585876
Chanyoung <33cksdud@gmail.com>
2025-07-21 12:24:31 +0900

    create

---
+++ src/main/java/com/example/chanai/ChanaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.chanai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChanaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chanai/ChanaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chanai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
